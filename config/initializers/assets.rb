a3ac1198246a41a8978e361ced4e5e2e2268030b
Tais Silva <tais.crsilva@gmail.com>
2018-11-26 23:22:13 +0000

    Precompile do calendar.css em assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,6 +20,7 @@ Rails.application.config.assets.precompile += %w( cable.js
                                                   login.js 
                                                   calendar.js
                                                   devise/sessions.js)
+                                                  #26/11/2018 calendar
                                                 
 Rails.application.config.assets.precompile += %w( predios.css
                                                   application.css
