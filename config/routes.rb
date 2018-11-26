12d707f80ec32943e48902aa3326fe2bd1f7e200
Franciele <franciele_zanella@hotmail.com>
2018-11-26 18:45:47 +0000

    autenticação de sindico com devise

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
   resources :predios
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  #rota pagina inicial
    root 'start#home'
 end
