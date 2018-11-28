f657b11b5d20329b860df8925225eefe73f0567e
Tais Silva <tais.crsilva@gmail.com>
2018-11-28 20:32:50 +0000

    events.coffee para events.js

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,48 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+var initialize_calendar;
+initialize_calendar = function() {
+  $('.calendar').each(function(){
+    var calendar = $(this);
+    calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true,
+      events: '/events.json',
+
+      select: function(start, end) {
+        $.getScript('/events/new', function() {});
+
+        calendar.fullCalendar('unselect');
+      },
+
+      eventDrop: function(event, delta, revertFunc) {
+        event_data = { 
+          event: {
+            id: event.id,
+            start: event.start.format(),
+            end: event.end.format()
+          }
+        };
+        $.ajax({
+            url: event.update_url,
+            data: event_data,
+            type: 'PATCH'
+        });
+      },
+      
+      eventClick: function(event, jsEvent, view) {
+        $.getScript(event.edit_url, function() {});
+      }
+    });
+  })
+};
+$(document).on('turbolinks:load', initialize_calendar);
