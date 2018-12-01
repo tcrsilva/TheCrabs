d924b5ce2b13ee9b6c89cbe58d399a76fa14adb1
Juan Bertoluzzi Garcia <juanbertoluzzigarcia@gmail.com>
2018-12-01 00:05:22 +0000

    alteração de design, botao ruby e bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   resources :eventos
 
+
   resources :information
   resources :events
   get 'calendar/schedule'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_11_30_193048) do
-
+ActiveRecord::Schema.define(version: 2018_11_30_224849) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -102,4 +100,3 @@ ActiveRecord::Schema.define(version: 2018_11_30_193048) do
   end
 
 end
-
