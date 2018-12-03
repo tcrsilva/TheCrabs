181c5cfa42aed4e991a6d22dffdb3c1b4b907623
Tais Silva <tais.crsilva@gmail.com>
2018-12-03 14:08:50 +0000

    Configurações para teste em rspec em modelsapplication e primeiro teste com Eventos -> OK!

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,2 +1,4 @@
 class Evento < ApplicationRecord
+    validates :local, presence: true
+    validates :date, presence: true
 end
---
+++ spec/models/evento_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Evento, type: :model do
+    context "Validade de um Evento" do
+        it "Evento valido?" do
+            evento = Evento.new
+            evento.titulo = "Aniversário Júlio AP 20"
+            evento.texto = "Venha comemorar meu aniversário!"
+            evento.date = "2018-11-30"
+            evento.local = "Salão de Festas"
+            expect(evento.valid?).to be_truthy
+        end
+        
+        it "Evento sem data e local?" do
+            evento = Evento.new
+            evento.date = nil
+            evento.local = nil
+            expect(evento.valid?).to be_falsey
+        end
+        
+        it "Evento valido?" do
+            evento1 = Evento.new
+            evento1.titulo = "Reunião"
+            evento1.texto = "Discutiremos sobre a reforma"
+            evento1.date = "2018-11-17"
+            evento1.local = "Sala de Reuniões no Terraço. Aguardo por você!"
+            expect(evento1.valid?).to be_truthy
+        end
+        
+        it "Evento valido?" do
+            evento2 = Evento.new
+            evento2.titulo = "Aniversário Bianca AP12"
+            evento2.texto = "Venha comemorar o 1º aniversário da minha filha!! Traga a criançada! Começará as 14h"
+            evento2.date = "2018-11-11"
+            evento2.local = "Salão de Festas"
+            expect(evento2.valid?).to be_truthy
+        end
+    end
+end
